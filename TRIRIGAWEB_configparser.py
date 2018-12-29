d1f208933dd40da7fea9d8b9619e92f81600f47b
mikem2314 <mikem2314@gmail.com>
2018-12-28 22:20:13 -0500

    initial commit

---
+++ TRIRIGAWEB_configparser.py
@@ -0,0 +1,23 @@
+def get_SSO():
+    tririgaVars = {}
+    with open("TRIRIGAWEB.properties") as propFile:
+        for line in propFile:
+            if "#" not in line:
+                if "SSO_" in line:
+                    param, value = line.partition("=")[::2]
+                    tririgaVars[param.strip()] = value
+
+    for k, v in tririgaVars.items():
+        print (k,v)
+
+"""
+for k, v in tririgaVars.items():
+    if "Y" in v:
+        print (k)
+        print (v)
+"""
+
+"""
+if "Y" in tririgaVars.get("SSO_REMOTE_USER"):
+    print ("yes")
+"""
