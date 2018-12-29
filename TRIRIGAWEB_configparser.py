75b86dcea2573829ae58b0c0b5a506d361a163f9
mikem2314 <mikem2314@gmail.com>
2018-12-28 22:33:55 -0500

    error handling for file open

--- TRIRIGAWEB_configparser.py
+++ TRIRIGAWEB_configparser.py
@@ -1,14 +1,18 @@
 def get_SSO():
     tririgaVars = {}
-    with open("TRIRIGAWEB.properties") as propFile:
-        for line in propFile:
-            if "#" not in line:
-                if "SSO_" in line:
-                    param, value = line.partition("=")[::2]
-                    tririgaVars[param.strip()] = value
+    try:
+        with open("tririgaWeb-Parser/TRIRIGAWEB.properties") as propFile:
+            for line in propFile:
+                if "#" not in line:
+                    if "SSO_" in line:
+                        param, value = line.partition("=")[::2]
+                        tririgaVars[param.strip()] = value
 
-    for k, v in tririgaVars.items():
-        print (k,v)
+        for k, v in tririgaVars.items():
+            print (k,v)
+    except IOError:
+        print ("File not found")
+
 
 get_SSO()
 """
