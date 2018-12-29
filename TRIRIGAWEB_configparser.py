52a9b3102df64973f80764fde11c3119f6f74a48
mikem2314 <mikem2314@gmail.com>
2018-12-28 22:59:44 -0500

    removed test functions/code

--- TRIRIGAWEB_configparser.py
+++ TRIRIGAWEB_configparser.py
@@ -30,12 +30,6 @@ def get_WFAgent():
                     if any(parm in line for parm in agentParms):
                         param, value = line.partition("=")[::2]
                         agentVars[param.strip()] = value
-                    """if "WFAgent" in line:
-                        param, value = line.partition("=")[::2]
-                        agentVars[param.strip()] = value
-                    if "WF_AGENT" in line:
-                        param, value = line.partition("=")[::2]
-                        agentVars[param.strip()] = value"""
         for k, v in agentVars.items():
             print (k,v)
     except IOError:
@@ -46,14 +40,3 @@ get_SSO()
 
 print ("\nDumping workflow agent related key/value pairs:\n\n")
 get_WFAgent()
-"""
-for k, v in tririgaVars.items():
-    if "Y" in v:
-        print (k)
-        print (v)
-"""
-
-"""
-if "Y" in tririgaVars.get("SSO_REMOTE_USER"):
-    print ("yes")
-"""
